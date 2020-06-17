a68f5695c3152561542fc736c2334031af5f2b1e
Mksiyak <mukeshsiyak002@gmail.com>
2020-06-17 12:59:13 +0530

    basic part done

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const { mongoose } = require('./config.js');
+
+var controller = require('./controller/controller.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.listen(3000, () => console.log('Server started at port : 3000'));
+
+app.use('/employees', controller);
